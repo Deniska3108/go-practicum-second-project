c6a8052d757bb88e3fe1d9b3eb7f03ab62d8ec78
Денис Гутенюк <denisgutenyuk@gmail.com>
2026-02-26 09:59:45 +0300

    first commit

--- internal/actioninfo/actioninfo.go
+++ internal/actioninfo/actioninfo.go
@@ -1,9 +1,33 @@
 package actioninfo
 
+import (
+	"fmt"
+	"log"
+)
+
 type DataParser interface {
 	// TODO: добавить методы
+	Parse(string) error
+	ActionInfo() (string, error)
 }
 
 func Info(dataset []string, dp DataParser) {
 	// TODO: реализовать функцию
+	var check bool
+	for _, v := range dataset {
+		err := dp.Parse(v)
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+		check = true
+	}
+	if !check {
+		return
+	}
+	f, err := dp.ActionInfo()
+	if err != nil {
+		log.Println(err)
+	}
+	fmt.Println(f)
 }
--- internal/spentenergy/spentenergy.go
+++ internal/spentenergy/spentenergy.go
@@ -1,6 +1,7 @@
 package spentenergy
 
 import (
+	"errors"
 	"time"
 )
 
@@ -14,16 +15,51 @@ const (
 
 func WalkingSpentCalories(steps int, weight, height float64, duration time.Duration) (float64, error) {
 	// TODO: реализовать функцию
+	if steps <= 0 {
+		return 0, errors.New("не удалось получить количество шагов")
+	}
+	if weight <= 0 {
+		return 0, errors.New("не удалось получить вес пользователя")
+	}
+	if height <= 0 {
+		return 0, errors.New("не удалось получить рост пользователя")
+	}
+	if duration <= 0 {
+		return 0, errors.New("не удалось получить время прогулки")
+	}
+
+	calories := (weight * MeanSpeed(steps, height, duration) * duration.Minutes()) / minInH
+
+	return calories * walkingCaloriesCoefficient, nil
 }
 
 func RunningSpentCalories(steps int, weight, height float64, duration time.Duration) (float64, error) {
 	// TODO: реализовать функцию
+	if steps <= 0 {
+		return 0, errors.New("не удалось получить количество шагов")
+	}
+	if weight <= 0 {
+		return 0, errors.New("не удалось получить вес пользователя")
+	}
+	if height <= 0 {
+		return 0, errors.New("не удалось получить рост пользователя")
+	}
+	if duration <= 0 {
+		return 0, errors.New("не удалось получить время бега")
+	}
+
+	return (weight * MeanSpeed(steps, height, duration) * duration.Minutes()) / minInH, nil
 }
 
 func MeanSpeed(steps int, height float64, duration time.Duration) float64 {
 	// TODO: реализовать функцию
+	if duration <= 0 {
+		return 0
+	}
+	return Distance(steps, height) / duration.Hours()
 }
 
 func Distance(steps int, height float64) float64 {
 	// TODO: реализовать функцию
+	return (height * stepLengthCoefficient * float64(steps)) / mInKm
 }
